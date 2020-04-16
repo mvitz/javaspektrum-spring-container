39f1477a95f631f4df47d72e8ace7840dbb540b3
Michael Vitz <michael.vitz@innoq.com>
2020-04-16 10:03:32 +0200

    Example: Add start.spring.io generated application

---
+++ example/src/test/java/de/mvitz/spring/container/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mvitz.spring.container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
